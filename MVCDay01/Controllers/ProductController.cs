b1a65682362a77472a6bf8aac03873fd46dd61e8
KhloudHisham <khokhakammar@gmail.com>
2024-02-29 03:05:08 +0200

    Initial commit

---
+++ MVCDay01/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDay01.Models;
+
+namespace MVCDay01.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult All()
+        {
+            ProductBL productBL = new ProductBL();
+            List<Product> productList= productBL.GetAll();
+            return View("All", productList);
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            ProductBL productBL = new ProductBL();
+            Product productModel = productBL.GetByID(id);  
+
+           
+            return View("Details", productModel);
+        }
+
+    }
+}
---
+++ MVCDay01/Models/ProductBL.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace MVCDay01.Models
+{
+    public class ProductBL
+    {
+        List<Product> Products = new List<Product>();
+        public ProductBL()
+        {
+            Products.Add(new Product() { Id = 1, Name = "Camera", Price = 1200, Image = "camera.jpg" });
+            Products.Add(new Product() { Id = 2, Name = "Phone", Price = 9000, Image = "phone.jpg"});
+         Products.Add(new Product() { Id = 3, Name = "labtop", Price = 20000, Image = "labtop.jpg" });
+            Products.Add(new Product() { Id = 4, Name = "Microfon", Price = 1000, Image = "microfon.jpg" });
+            Products.Add(new Product() { Id = 5, Name = "Alarm", Price = 9000, Image = "alarm.jpg" });        }
+        public List<Product> GetAll() {
+            return Products;
+        }
+        public Product GetByID(int id)
+        {
+            return Products.FirstOrDefault(e => e.Id == id);
+        }
+
+    }
+}
+
